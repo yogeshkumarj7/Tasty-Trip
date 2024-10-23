90f6e11cdb13bc190b4ec90280f1ac66101de523
Yogesh <coolcoder9334@gmail.com>
2024-10-23 22:02:40 +0530

    created the environment for the project

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const App = () => {
+  return <h1>Welcome to Beed City..!</h1>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
