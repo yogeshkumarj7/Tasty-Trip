6f16b65a80a77fea89961984bf6dc354ad3975b8
Yogesh <coolcoder9334@gmail.com>
2024-10-23 23:51:25 +0530

    Done with the header

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import TRIP from "./images/TRIP.png";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={TRIP} alt="logo" />
+      </div>
+      <div className="nav-items">
+        <ul className="list">
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
 const App = () => {
-  return <h1>Welcome to Beed City..!</h1>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
