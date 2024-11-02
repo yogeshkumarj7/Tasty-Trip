fc3ed4048d9cc934374d92ec088f4fc27eecb71e
Yogesh <coolcoder9334@gmail.com>
2024-11-02 21:17:50 +0530

    Added a simple loading carts

---
+++ src/components/Loading.js
@@ -0,0 +1,9 @@
+export const Loading = () => {
+  return (
+    <div className="loading-container">
+      <div className="loading-card"></div>
+      <div className="loading-card"></div>
+      <div className="loading-card"></div>
+    </div>
+  );
+};
