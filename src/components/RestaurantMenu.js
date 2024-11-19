2bce47903140a9be76e4e3b8b568aec59cfc44ed
Yogesh <coolcoder9334@gmail.com>
2024-11-19 21:28:17 +0530

    Restaurant menu started building

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -5,29 +5,35 @@ import { Loading } from "./Loading";
 import { RES_API_URL_1 } from "../utils/constants";
 import { useParams } from "react-router-dom";
 import useMenuData from "../utils/useMenuData";
+import RestaurantCategory from "./RestaurantCategory";
 const RestaurantMenu = () => {
   const { resId } = useParams();
   const menuData = useMenuData(resId);
 
   const { name, cuisines, id, costForTwo } =
     menuData?.cards[2]?.card?.card?.info || {};
+  // const { groupcard } = menuData?.cards[5]?.card?.card?.info || {};
+  // console.log(menuData?.cards[5].groupedCard?.cardGroupMap?.REGULAR?.cards);
+
+  const filterCategories =
+    menuData?.cards[4].groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+      (c) =>
+        c.card?.card?.["@type"] ==
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
+  console.log(filterCategories);
+
   if (menuData == null) {
     return <Loading />;
   }
   return (
-    <div>
-      <h1>{name}</h1>
+    <div className=" text-center">
+      <h1 className="font-bold  ">{name}</h1>
       <p>{cuisines.join(" ,")}</p>
-      <h3>{costForTwo}</h3>
-      <h2>Menu</h2>
-      {/* <ul>
-        {itemCards.slice(0, 10).map((resName, index) => (
-          <li key={resName.card.info.id}>
-            {resName.card.info.name}-{"Rs "}
-            {resName.card.info.price / 100}
-          </li>
-        ))}
-      </ul> */}
+      {/* Categories Accordions */}
+      {filterCategories.map((category, key) => (
+        <RestaurantCategory data={category?.card?.card} />
+      ))}
     </div>
   );
 };
