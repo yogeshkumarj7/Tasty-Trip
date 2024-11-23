14205d5f1fa48b05b03d0c3cd5fd76fcb6baa133
Yogesh <coolcoder9334@gmail.com>
2024-11-23 23:46:45 +0530

    created the restaurant categories

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,20 +1,39 @@
 import React from "react";
-
+import CategoryList from "./CategoryList";
 const RestaurantCategory = ({ data }) => {
-  console.log(data);
-  const { title, itemCards } = data;
-  console.log(itemCards);
+//   console.log(data);
+
   return (
-    <div className="">
-      {/* Header */}
-      <div className="my-3">
-        <span>{title}</span>
-        <span>
-          <i className="fa-solid fa-chevron-down"></i>
-        </span>
+    <div>
+      <div className="w-6/12 mx-auto rounded-lg bg-violet-300 my-3 flex p-4 cursor-pointer">
+        <div className="flex justify-between">
+          <span className="font-bold">
+            
+            {data.title} ({data.itemCards.length}) 
+          </span>
+          <span>
+            <i className="fa-solid fa-chevron-down"></i> 
+          </span>
+        </div>
       </div>
+      <CategoryList items={data.itemCards} />
     </div>
   );
 };
 
+{
+  /* <div>
+      <div className="w-6/12 p-2 mx-auto rounded-lg bg-violet-300 my-3 flex p-4 cursor-pointer">
+        <div className="flex justify-between">
+          <span className="font-bold">
+            {title} ({itemCards.length})
+          </span>
+          <span>
+            <i className="fa-solid fa-chevron-down"></i>
+          </span>
+        </div>
+        <CategoryList data={data.itemCards} />
+      </div>
+    </div> */
+}
 export default RestaurantCategory;
