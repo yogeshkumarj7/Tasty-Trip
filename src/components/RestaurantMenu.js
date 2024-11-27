7ed996da39d0d2b007659575368ca6c9808552d3
Yogesh <coolcoder9334@gmail.com>
2024-11-27 21:54:46 +0530

    added the key to the Menu

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -9,31 +9,34 @@ import RestaurantCategory from "./RestaurantCategory";
 const RestaurantMenu = () => {
   const { resId } = useParams();
   const menuData = useMenuData(resId);
-
+  // console.log(menuData);
   const { name, cuisines, id, costForTwo } =
     menuData?.cards[2]?.card?.card?.info || {};
-  // const { groupcard } = menuData?.cards[5]?.card?.card?.info || {};
-  // console.log(menuData?.cards[5].groupedCard?.cardGroupMap?.REGULAR?.cards);
-
   const filterCategories =
     menuData?.cards[4].groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
       (c) =>
         c.card?.card?.["@type"] ==
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
     );
+  //4  5
   console.log(filterCategories);
 
   if (menuData == null) {
     return <Loading />;
   }
   return (
-    <div className=" text-center">
-      <h1 className="font-bold  ">{name}</h1>
-      <p>{cuisines.join(" ,")}</p>
+    <div className="bg-white rounded-lg shadow-lg p-8 text-center">
+      <h1 className="text-2xl font-bold text-gray-800 mb-2  ">{name}</h1>
+      <p className="text-gray-600 mb-6">{cuisines.join(" ,")}</p>
       {/* Categories Accordions */}
-      {filterCategories.map((category, key) => (
-        <RestaurantCategory data={category?.card?.card} />
-      ))}
+      <div className="space-y-6">
+        {filterCategories.map((category, key) => (
+          <RestaurantCategory
+            key={category?.card?.card.title}
+            data={category?.card?.card}
+          />
+        ))}
+      </div>
     </div>
   );
 };
