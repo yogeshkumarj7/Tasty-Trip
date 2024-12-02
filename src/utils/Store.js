feac1e6a810043cd9340b93a399b2f92304c627e
Yogesh <coolcoder9334@gmail.com>
2024-12-02 23:37:48 +0530

    created the slice for managing the cart section

---
+++ src/utils/Store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./cartSlice";
+const Store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
+});
+
+export default Store;
