6f668318686f1d32378288ecd2acd1e85bd155ea
Yogesh <coolcoder9334@gmail.com>
2025-01-08 22:30:14 +0530

    created slice for simple auth

---
+++ src/utils/authSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    isLoggedIn: false,
+    username: "",
+  },
+  reducers: {
+    login: (state, action) => {
+      state.isLoggedIn = true;
+      state.username = action.payload;
+    },
+    logout: (state) => {
+      state.isLoggedIn = false;
+      state.username = "";
+    },
+  },
+});
+
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
