e52f5dddc06e5a83e21d283718cd6bee7305334a
Yogesh <coolcoder9334@gmail.com>
2025-01-08 22:32:29 +0530

    updated store for authSlice

--- src/utils/Store.js
+++ src/utils/Store.js
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartSlice";
+import authReducer from "./authSlice";
+
 const Store = configureStore({
   reducer: {
     cart: cartReducer,
+    auth: authReducer,
   },
 });
 
-export default Store; 
+export default Store;
