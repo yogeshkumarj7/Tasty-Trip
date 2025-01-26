dbd34feb6da0524c421c656165d57f36d898be13
Yogesh <coolcoder9334@gmail.com>
2025-01-26 20:00:59 +0530

    Cleard unwanted code

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,51 +1,6 @@
 export const GITHUB_USER_API = "https://api.github.com/users/yogeshkumarj7";
-
-//.....................
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-// export const RES_API_URL =
-//   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.990088&lng=75.7531324&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
-
-// export const RES_API_URL_1 =
-//   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.990088&lng=75.7531324&restaurantId=";
-//..//..//
 export const RES_API_URL =
   "https://tasty-trip-server.onrender.com/api/restaurants";
 export const RES_API_URL_1 = "https://tasty-trip-server.onrender.com/api/menu/";
-
-//..............................................
-// export const CDN_URL =
-//   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-// export const RES_API_URL =
-//   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.990088&lng=75.7531324&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
-
-// export const RES_API_URL_1 =
-//   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.990088&lng=75.7531324&restaurantId=";
-
-// export const CDN_URL = `https://cors-anywhere.herokuapp.com/https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/`;
-// export const RES_API_URL = `https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.990088&lng=75.7531324&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
-// export const RES_API_URL_1 = `https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.990088&lng=75.7531324&restaurantId=`;
-
-//.....................
-// export const CDN_URL =
-//   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-// export const RES_API_URL =
-//   "restaurants/list/v5?lat=18.990088&lng=75.7531324&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
-
-// export const RES_API_URL_1 =
-//   "menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.990088&lng=75.7531324&restaurantId=";
-
-//.......................
-// export const CDN_URL = `${process.env.REACT_MEDIA_ASSETS_BASE_URL}image/upload/fl_lossy,f_auto,q_auto,w_660/`;
-
-// export const RES_API_URL = `${process.env.REACT_TASTY_TRIP_APP_BASE_URL}restaurants/list/v5?lat=18.990088&lng=75.7531324&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
-
-// export const RES_API_URL_1 = `${process.env.REACT_TASTY_TRIP_APP_BASE_URL}menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.990088&lng=75.7531324&restaurantId=`;
-
-// export const CDN_URL = `${process.env.REACT_MEDIA_ASSETS_BASE_URL}image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/`;
-
-// // Swiggy API to get Restaurant data using foodfire server
-// export const RES_API_URL = `${process.env.REACT_TASTY_TRIP_APP_BASE_URL}restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING`;
-
-// // Swiggy API to get Restaurant Menu data using foodfire server
-// export const RES_API_URL_1 = `${process.env.REACT_TASTY_TRIP_APP_BASE_URL}menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=`;
