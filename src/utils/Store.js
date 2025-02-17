bbcd50d2286e8b14bd90cb0ab447da4f3e831642
Yogesh <coolcoder9334@gmail.com>
2025-02-17 21:49:24 +0530

    Included loading slice in store and updated authSlice

--- src/utils/Store.js
+++ src/utils/Store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartSlice";
 import authReducer from "./authSlice";
+import loaderReducer from "./loaderSlice";
 
 const Store = configureStore({
   reducer: {
     cart: cartReducer,
     auth: authReducer,
+    loader: loaderReducer,
   },
 });
 
