240a4c581339b5fde1580ee37ee3e7527440324a
Yogesh <coolcoder9334@gmail.com>
2025-02-23 22:12:55 +0530

    Added themeReducer in store

--- src/utils/Store.js
+++ src/utils/Store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartSlice";
 import authReducer from "./authSlice";
 import loaderReducer from "./loaderSlice";
+import themeReducer from "./themeSlice";
 
 const Store = configureStore({
   reducer: {
     cart: cartReducer,
     auth: authReducer,
     loader: loaderReducer,
+    theme: themeReducer,
   },
 });
 
